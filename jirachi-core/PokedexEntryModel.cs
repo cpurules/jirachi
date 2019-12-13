75f9ecabe176f7520913aed791c8e08c55a0e3f5
Charlie Unfricht <cunfricht@harbourvest.com>
2019-12-13 13:09:59 -0500

    Create PokedexEntryModel

---
+++ jirachi-core/PokedexEntryModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jirachi_core {
+    class PokedexEntryModel {
+        public int NationalDexId { get; set; }
+        public bool SeenPokemon { get; set; }
+        public bool OwnedPokemon { get; set; }
+    }
+}
