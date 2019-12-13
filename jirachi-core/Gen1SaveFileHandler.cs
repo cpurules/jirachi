4e4c738239c6ecc8d6bddfd5bf94dcd10c7bdbaf
Charlie Unfricht <cunfricht@harbourvest.com>
2019-12-13 13:55:50 -0500

    Create Gen1SaveFileHandler class

---
+++ jirachi-core/Gen1SaveFileHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jirachi_core {
+    class Gen1SaveFileHandler : ISaveFileHandler {
+        public GameModel ReadSaveFile(string filePath) {
+            throw new NotImplementedException();
+        }
+
+        public void WriteSaveFile(string filePath) {
+            throw new NotImplementedException();
+        }
+    }
+}
