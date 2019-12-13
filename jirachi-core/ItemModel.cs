89a30242376c8ec5cd2d171bf9981dcff5522158
Charlie Unfricht <cunfricht@harbourvest.com>
2019-12-13 12:34:44 -0500

    Create ItemModel

---
+++ jirachi-core/ItemModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jirachi_core {
+    class ItemModel {
+        /// <summary>
+        /// Represents the generation-specific item index
+        /// </summary>
+        public int ItemId { get; set; }
+        /// <summary>
+        /// Represents the generation this item ID is linked to
+        /// </summary>
+        public int Generation { get; set; }
+        /// <summary>
+        /// Represents the pocket that holds this item
+        /// </summary>
+        public ItemPocket Pocket { get; set; }
+        /// <summary>
+        /// Represents the quantity of this item
+        /// </summary>
+        public int Quantity { get; set; }
+    }
+}
