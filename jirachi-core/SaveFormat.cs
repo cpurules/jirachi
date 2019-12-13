11cdb8175af4cede12f6bd086899e8215330b25a
Charlie Unfricht <cunfricht@harbourvest.com>
2019-12-13 12:21:55 -0500

    Add SaveFormat enum to handle differing save data formats

---
+++ jirachi-core/SaveFormat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jirachi_core {
+    public enum SaveFormat {
+        Gen1RB,
+        Gen1Y,
+        Gen2GS,
+        Gen2C,
+        Gen3RS,
+        Gen3E,
+        Gen3FRLG
+    }
+}
