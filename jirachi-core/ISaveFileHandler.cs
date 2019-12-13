3cb2638f5a45cb28a35804dd7094ba0bfc83bf4e
Charlie Unfricht <cunfricht@harbourvest.com>
2019-12-13 13:55:01 -0500

    Create ISaveFileHandler interface

---
+++ jirachi-core/ISaveFileHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jirachi_core {
+    interface ISaveFileHandler {
+        void WriteSaveFile(string filePath);
+        GameModel ReadSaveFile(string filePath);
+    }
+}
