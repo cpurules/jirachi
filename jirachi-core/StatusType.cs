e5e1323e54274d768b48bc295ba8c03bc6523b5d
Charlie Unfricht <cunfricht@harbourvest.com>
2019-12-13 12:40:20 -0500

    Add StatusType enumeration

---
+++ jirachi-core/StatusType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jirachi_core {
+    public enum StatusType {
+        Burn,
+        Freeze,
+        Paralysis,
+        Poison,
+        Sleep
+    }
+}
