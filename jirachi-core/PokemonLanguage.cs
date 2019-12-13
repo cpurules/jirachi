b4154d130b0326f68a60d7681a9db6e620fbadcd
Charlie Unfricht <cunfricht@harbourvest.com>
2019-12-13 12:54:48 -0500

    Create PokemonLanguage enumeration

---
+++ jirachi-core/PokemonLanguage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jirachi_core {
+    public enum PokemonLanguage {
+        Japanese,
+        English,
+        French,
+        Italian,
+        German,
+        Korean,
+        Spanish
+    }
+}
