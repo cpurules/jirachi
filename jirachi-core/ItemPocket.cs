2efc59781b833d54f9b59da5ab8883c43e4940c1
Charlie Unfricht <cunfricht@harbourvest.com>
2019-12-13 12:32:43 -0500

    Add ItemPocket enum type

---
+++ jirachi-core/ItemPocket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jirachi_core {
+    public enum ItemPocket {
+        ItemPocket,
+        KeyItemPocket,
+        BallPocket,
+        TMHMPocket,
+        MedicinePocket,
+        BerryPocket,
+        BattleItemPocket,
+        MailPocket
+    }
+}
