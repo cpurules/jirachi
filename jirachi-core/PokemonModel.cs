e86a1d3fff7d0a8a325c9a64c233786811446157
Charlie Unfricht <cunfricht@harbourvest.com>
2019-12-13 12:26:45 -0500

    Add Moveset property to PokemonModel

---
+++ jirachi-core/PokemonModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jirachi_core {
+    class PokemonModel {
+        /// <summary>
+        /// Represents the national dex number of the Pokemon.
+        /// </summary>
+        public int NationalDexNumber { get; set; }
+        /// <summary>
+        /// Represents the Pokemon's nickname
+        /// </summary>
+        public string Nickname { get; set; }
+        /// <summary>
+        /// Represents the Pokemon's current (actual) level
+        /// </summary>
+        public int Level { get; set; }
+        /// <summary>
+        /// Represents the Pokemon's current XP earned
+        /// </summary>
+        public int XP { get; set; }
+        /// <summary>
+        /// Represents the Pokemon's current HP
+        /// </summary>
+        public int CurrentHP { get; set; }
+        /// <summary>
+        /// Represents the Pokemon's moveset
+        /// </summary>
+        public List<MoveModel> Moveset { get; set; }
+    }
+}
