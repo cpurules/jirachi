490d5509c128c69bf0bdb1869d7cdfd972384c72
Charlie Unfricht <cunfricht@harbourvest.com>
2019-12-13 12:44:00 -0500

    Create PokerusModel

---
+++ jirachi-core/PokerusModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jirachi_core {
+    class PokerusModel {
+        /// <summary>
+        /// Represents the strain value of this Pokerus infection
+        /// </summary>
+        public int Strain { get; set; }
+        /// <summary>
+        /// Represents the duration remaining on this Pokerus infection
+        /// </summary>
+        public int Duration { get; set; }
+    }
+}
