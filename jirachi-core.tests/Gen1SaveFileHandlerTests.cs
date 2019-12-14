7a8c2fe0ad102cd33d5fc620dec7eab313491010
Charlie Unfricht <dzcharlie@gmail.com>
2019-12-14 00:18:00 -0500

    Create tests for Gen1SaveFileHandler

---
+++ jirachi-core.tests/Gen1SaveFileHandlerTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xunit;
+using jirachi_core;
+
+namespace jirachi_core.tests {
+    public class Gen1SaveFileHandlerTests {
+        string pathToDemoSave = "C:\\Users\\cpuru\\Downloads\\TEST_BLUE_SAVE.sav";
+        // this demo save will always have the same data
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldCreate() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldThrowFileNotFound() {
+            Assert.Throws<FileNotFoundException>(() => new Gen1SaveFileHandler("THIS_IS_A_FAKE_PATH"));
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldThrowNotSave() {
+            string pathToNonSaveFile = "C:\\key.txt";
+
+            Assert.Throws<FormatException>(() => new Gen1SaveFileHandler(pathToNonSaveFile));
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadTrainerID() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+            GameModel Gen1Game = Gen1Save.ReadSaveFile();
+
+            Assert.Equal(20893, Gen1Game.TrainerID);
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadMoney() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+            GameModel Gen1Game = Gen1Save.ReadSaveFile();
+
+            Assert.Equal(980399, Gen1Game.Money);
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadCoins() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+            GameModel Gen1Game = Gen1Save.ReadSaveFile();
+
+            Assert.Equal(50, Gen1Game.Coins);
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadHours() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+            GameModel Gen1Game = Gen1Save.ReadSaveFile();
+
+            Assert.Equal(42, Gen1Game.HoursPlayed);
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadMins() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+            GameModel Gen1Game = Gen1Save.ReadSaveFile();
+
+            Assert.Equal(42, Gen1Game.MinsPlayed);
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadSecs() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+            GameModel Gen1Game = Gen1Save.ReadSaveFile();
+
+            Assert.Equal(44, Gen1Game.SecsPlayed);
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadFrames() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+            GameModel Gen1Game = Gen1Save.ReadSaveFile();
+
+            Assert.Equal(44, Gen1Game.FramesPlayed);
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadRivalName() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+            GameModel Gen1Game = Gen1Save.ReadSaveFile();
+
+            Assert.Equal("GARY", Gen1Game.RivalName);
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadTrainerName() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+            GameModel Gen1Game = Gen1Save.ReadSaveFile();
+
+            Assert.Equal("ASH", Gen1Game.TrainerName);
+        }
+    }
+}
