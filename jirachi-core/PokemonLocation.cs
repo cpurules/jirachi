48230b3fe6cd16924d6faea70ffd6499153d9401
Charlie Unfricht <dzcharlie@gmail.com>
2019-12-14 14:21:50 -0500

    Create PokemonLocation enum

---
+++ jirachi-core/PokemonLocation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jirachi_core {
+    public enum PokemonLocation {
+        Party,
+        Box,
+        Daycare
+    }
+}
