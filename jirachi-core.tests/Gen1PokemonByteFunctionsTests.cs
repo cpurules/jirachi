e314a0a84ff2e8b0c3b6619a9a65afbe2b537849
Charlie Unfricht <dzcharlie@gmail.com>
2019-12-15 00:54:07 -0500

    Refactor Gen 1 Pokemon byte functions to their own class and test class

---
+++ jirachi-core.tests/Gen1PokemonByteFunctionsTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using jirachi_core;
+using Xunit;
+
+namespace jirachi_core.tests {
+    public class Gen1PokemonByteFunctionsTests {
+        string pathToDemoSave = "C:\\Users\\cpuru\\Downloads\\TEST_BLUE_SAVE.sav";
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadCurrentHP() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+
+            // Box 12 starts at 0x75EA
+            byte[] magmarBox12 = new byte[33];
+            Array.Copy(Gen1Save.saveFileBytes, 0x75EA + 0x16, magmarBox12, 0, 33);
+            int magmarBox12Expected = 179;
+            int magmarBox12Actual = Gen1PokemonByteFunctions.ReadCurrentHPFromPkmnBytes(magmarBox12);
+
+            // Party starts at 0x2F2C
+            byte[] mewPartySlot1 = new byte[44];
+            Array.Copy(Gen1Save.saveFileBytes, 0x2F2C + 0x8, mewPartySlot1, 0, 44);
+            int mewPartySlot1Expected = 260;
+            int mewPartySlot1Actual = Gen1PokemonByteFunctions.ReadCurrentHPFromPkmnBytes(mewPartySlot1);
+
+            Assert.Equal(magmarBox12Expected, magmarBox12Actual);
+            Assert.Equal(mewPartySlot1Expected, mewPartySlot1Actual);
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadLevel() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+
+            // Box 6 starts at 0x55EA
+            byte[] persianBox6 = new byte[33];
+            Array.Copy(Gen1Save.saveFileBytes, 0x55EA + 0x16 + (33 * 2), persianBox6, 0, 33);
+            int persianExpected = 70;
+            int persianActual = Gen1PokemonByteFunctions.ReadLevelFromPkmnBytes(persianBox6);
+
+            Assert.Equal(persianExpected, persianActual);
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadStatus() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+
+            // We're going to write these later...
+            // PkHex doesn't display the status so will need to load up the game..
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadMoveset() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+
+            byte[] magmarBox12 = new byte[33];
+            Array.Copy(Gen1Save.saveFileBytes, 0x75EA + 0x16, magmarBox12, 0, 33);
+
+            List<MoveModel> magmarBox12Expected = new List<MoveModel>();
+            magmarBox12Expected.Add(new MoveModel(52, 25, 0));
+            magmarBox12Expected.Add(new MoveModel(126, 5, 0));
+            magmarBox12Expected.Add(new MoveModel(53, 15, 0));
+            magmarBox12Expected.Add(new MoveModel(7, 15, 0));
+            List<MoveModel> magmarBox12Actual = Gen1PokemonByteFunctions.ReadMovesetFromPkmnBytes(magmarBox12);
+
+
+            byte[] mewPartySlot1 = new byte[44];
+            Array.Copy(Gen1Save.saveFileBytes, 0x2F2C + 0x8, mewPartySlot1, 0, 44);
+
+            List<MoveModel> mewPartyExpected = new List<MoveModel>();
+            mewPartyExpected.Add(new MoveModel(105, 32, 3));
+            mewPartyExpected.Add(new MoveModel(5, 32, 3));
+            mewPartyExpected.Add(new MoveModel(94, 16, 3));
+            mewPartyExpected.Add(new MoveModel(144, 16, 3));
+            List<MoveModel> mewPartyActual = Gen1PokemonByteFunctions.ReadMovesetFromPkmnBytes(mewPartySlot1);
+
+            Assert.Equal(magmarBox12Expected, magmarBox12Actual);
+            Assert.Equal(mewPartyExpected, mewPartyActual);
+
+
+            byte[] magmarDaycare = new byte[33];
+            Array.Copy(Gen1Save.saveFileBytes, 0x2D0B, magmarDaycare, 0, 33);
+
+            List<MoveModel> magmarDaycareExpected = new List<MoveModel>();
+            magmarDaycareExpected.Add(new MoveModel(52, 25, 0));
+            List<MoveModel> magmarDaycareActual = Gen1PokemonByteFunctions.ReadMovesetFromPkmnBytes(magmarDaycare);
+
+            Assert.Equal(magmarDaycareExpected, magmarDaycareActual);
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadOTID() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+
+            byte[] magmarBox12 = new byte[33];
+            Array.Copy(Gen1Save.saveFileBytes, 0x75EA + 0x16, magmarBox12, 0, 33);
+            int magmarBoxExpected = 20893;
+            int magmarBoxActual = Gen1PokemonByteFunctions.ReadOTIDFromPkmnBytes(magmarBox12);
+            Assert.Equal(magmarBoxExpected, magmarBoxActual);
+
+            byte[] machampBox12 = new byte[33];
+            Array.Copy(Gen1Save.saveFileBytes, 0x75EA + 0x16 + 33, machampBox12, 0, 33);
+            int machampBoxExpected = 17178;
+            int machampBoxActual = Gen1PokemonByteFunctions.ReadOTIDFromPkmnBytes(machampBox12);
+            Assert.Equal(machampBoxExpected, machampBoxActual);
+        }
+
+        [Fact]
+        public void Gen1SaveFileHandler_ShouldReadXP() {
+            Gen1SaveFileHandler Gen1Save = new Gen1SaveFileHandler(this.pathToDemoSave);
+
+            byte[] magmarBox12 = new byte[33];
+            Array.Copy(Gen1Save.saveFileBytes, 0x75EA + 0x16, magmarBox12, 0, 33);
+            int magmarBoxExpected = 274925;
+            int magmarBoxActual = Gen1PokemonByteFunctions.ReadXPFromPkmnBytes(magmarBox12);
+            Assert.Equal(magmarBoxExpected, magmarBoxActual);
+
+            byte[] persianBox6 = new byte[33];
+            Array.Copy(Gen1Save.saveFileBytes, 0x55EA + 0x16 + (33 * 2), persianBox6, 0, 33);
+            int persianExpected = 343000;
+            int persianActual = Gen1PokemonByteFunctions.ReadXPFromPkmnBytes(persianBox6);
+            Assert.Equal(persianExpected, persianActual);
+        }
+    }
+}
--- jirachi-core/Gen1SaveFileHandler.cs
+++ jirachi-core/Gen1SaveFileHandler.cs
@@ -266,16 +266,16 @@ namespace jirachi_core {
             }
 
             // national dex id
-            nationalDexNumber = Gen1SaveFileHandler.ReadNationalDexNumberFromPkmnBytes(pokemonDataBytes);
+            nationalDexNumber = Gen1PokemonByteFunctions.ReadNationalDexNumberFromPkmnBytes(pokemonDataBytes);
 
             // current HP
-            currentHP = Gen1SaveFileHandler.ReadCurrentHPFromPkmnBytes(pokemonDataBytes);
+            currentHP = Gen1PokemonByteFunctions.ReadCurrentHPFromPkmnBytes(pokemonDataBytes);
 
             // level
-            level = Gen1SaveFileHandler.ReadLevelFromPkmnBytes(pokemonDataBytes);
+            level = Gen1PokemonByteFunctions.ReadLevelFromPkmnBytes(pokemonDataBytes);
 
             // status
-            status = Gen1SaveFileHandler.ReadStatusFromPkmnBytes(pokemonDataBytes);
+            status = Gen1PokemonByteFunctions.ReadStatusFromPkmnBytes(pokemonDataBytes);
 
 
 
@@ -284,87 +284,6 @@ namespace jirachi_core {
             return null;
         }
 
-        public static int ReadNationalDexNumberFromPkmnBytes(byte[] bytes) {
-            int gen1Index = bytes[0x0];
-
-            int nationalDexId = 0;
-            // lookup... to do...
-            return nationalDexId;
-        }
-
-        public static int ReadCurrentHPFromPkmnBytes(byte[] bytes) {
-            int currentHP = ByteFunctions.ReadBytesToInteger(bytes, 0x1, 2);
-            return currentHP;
-        }
-
-        public static int ReadLevelFromPkmnBytes(byte[] bytes) {
-            int level = bytes[0x3];
-            return level;
-        }
-
-        public static StatusType ReadStatusFromPkmnBytes(byte[] bytes) {
-            int status = bytes[0x4];
-            if (status == 0b0) {
-                return StatusType.None;
-            }
-            else if (status == 0b100) {
-                return StatusType.Sleep;
-            }
-            else if (status == 0b1000) {
-                return StatusType.Poison;
-            }
-            else if (status == 0b10000) {
-                return StatusType.Burn;
-            }
-            else if (status == 0b100000) {
-                return StatusType.Freeze;
-            }
-            else if (status == 0b1000000) {
-                return StatusType.Paralysis;
-            }
-            else {
-                throw new ArgumentOutOfRangeException("Unknown value for status type");
-            }
-        }
-
-        public static List<MoveModel> ReadMovesetFromPkmnBytes(byte[] bytes) {
-            // Move indexes are stored 0x08-0x0B and PP is stored at 0x1D-0x20
-
-            List<MoveModel> moveset = new List<MoveModel>();
-            for(int move = 0; move < 4; move++) {
-                int moveIndex = bytes[0x08 + move];
-                if(moveIndex == 0) {
-                    break;
-                }
-
-                int movePP = bytes[0x1D + move];
-                int currentPP = movePP & 0b111111;
-                int ppUps = (movePP & 0b11000000) >> 6;
-
-                moveset.Add(new MoveModel(moveIndex, currentPP, ppUps));
-            }
-
-            return moveset;
-        }
-
-        public static int ReadOTIDFromPkmnBytes(byte[] bytes) {
-            // OTID is at 0xC-0xD
-            int OTID = ByteFunctions.ReadBytesToInteger(bytes, 0xC, 2);
-            return OTID;
-        }
-
-        public static int ReadXPFromPkmnBytes(byte[] bytes) {
-            // XP is at 0x0E-0x10
-            int XP = ByteFunctions.ReadBytesToInteger(bytes, 0xE, 3);
-            return XP;
-        }
-
-        public static int ConvertIndexToNationalDex(int index) {
-            // source: https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_index_number_(Generation_I)
-
-            Dictionary<int, int> NationalDexLookup = new Dictionary<int, int>();
-
-            return 0; // stump
-        }
+        
     }
 }
