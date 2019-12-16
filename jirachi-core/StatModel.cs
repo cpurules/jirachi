d657333d3715ec66fdd1940fb4bb6049ecd0385c
Charlie Unfricht <dzcharlie@gmail.com>
2019-12-15 23:43:24 -0500

    Revert properties to only have actual value, since values are not stored at all for box pokemon

--- jirachi-core/StatModel.cs
+++ jirachi-core/StatModel.cs
@@ -5,8 +5,7 @@ using System.Text;
 namespace jirachi_core {
     public class StatModel {
         public StatType StatType { get; set; }
-        public int ActualValue { get; set; }
-        public int CalculatedValue { get; set; }
+        public int StatValue { get; set; }
         public int IV { get; set; }
         public int EV { get; set; }
     }
