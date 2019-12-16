ec9bbcf0848460cd419be25b8a45efc3bd0bb824
Charlie Unfricht <dzcharlie@gmail.com>
2019-12-15 23:35:41 -0500

    Refactor properties to have actual and calculated value

--- jirachi-core/StatModel.cs
+++ jirachi-core/StatModel.cs
@@ -5,7 +5,8 @@ using System.Text;
 namespace jirachi_core {
     public class StatModel {
         public StatType StatType { get; set; }
-        public int Value { get; set; }
+        public int ActualValue { get; set; }
+        public int CalculatedValue { get; set; }
         public int IV { get; set; }
         public int EV { get; set; }
     }
