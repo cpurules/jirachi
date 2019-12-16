00c3c118f8fd2ef45a68804c641bc0009c0b12fb
Charlie Unfricht <dzcharlie@gmail.com>
2019-12-15 23:32:15 -0500

    Create enum for stat types

---
+++ jirachi-core/StatType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jirachi_core {
+    public enum StatType {
+        HP,
+        Attack,
+        Defense,
+        Speed,
+        SpecialAttack,
+        SpecialDefense
+    }
+}
