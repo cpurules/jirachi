943b40550657a5ff571639153cc85818b3d7a89d
Charlie Unfricht <dzcharlie@gmail.com>
2019-12-15 23:32:58 -0500

    Create StatModel

---
+++ jirachi-core/StatModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace jirachi_core {
+    class StatModel {
+        public StatType StatType { get; set; }
+        public int Value { get; set; }
+        public int IV { get; set; }
+        public int EV { get; set; }
+    }
+}
