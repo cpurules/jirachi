1944a90385eda20fd3fa46a8a7b6a9342977e850
Charlie Unfricht <dzcharlie@gmail.com>
2019-12-15 23:49:04 -0500

    Create constructors for StatModel

--- jirachi-core/StatModel.cs
+++ jirachi-core/StatModel.cs
@@ -4,6 +4,20 @@ using System.Text;
 
 namespace jirachi_core {
     public class StatModel {
+        public StatModel(StatType statType, int statValue, int iV, int eV) {
+            StatType = statType;
+            StatValue = statValue;
+            IV = iV;
+            EV = eV;
+        }
+
+        public StatModel(StatType statType, int iV, int eV) {
+            StatType = statType;
+            StatValue = 0;
+            IV = iV;
+            EV = eV;
+        }
+
         public StatType StatType { get; set; }
         public int StatValue { get; set; }
         public int IV { get; set; }
