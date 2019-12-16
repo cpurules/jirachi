1e87cdfb47708de5bc3ddbad9a3c8243ce2c2f6c
Charlie Unfricht <dzcharlie@gmail.com>
2019-12-15 23:54:30 -0500

    Add overrides for Equals and GetHashCode

--- jirachi-core/StatModel.cs
+++ jirachi-core/StatModel.cs
@@ -22,5 +22,22 @@ namespace jirachi_core {
         public int StatValue { get; set; }
         public int IV { get; set; }
         public int EV { get; set; }
+
+        public override bool Equals(object obj) {
+            return obj is StatModel model &&
+                   StatType == model.StatType &&
+                   StatValue == model.StatValue &&
+                   IV == model.IV &&
+                   EV == model.EV;
+        }
+
+        public override int GetHashCode() {
+            var hashCode = 2043926891;
+            hashCode = hashCode * -1521134295 + StatType.GetHashCode();
+            hashCode = hashCode * -1521134295 + StatValue.GetHashCode();
+            hashCode = hashCode * -1521134295 + IV.GetHashCode();
+            hashCode = hashCode * -1521134295 + EV.GetHashCode();
+            return hashCode;
+        }
     }
 }
