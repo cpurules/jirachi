ba3a0b0bb9a307f363a053d66bd73db5f7c6fe86
cpurules <dzcharlie@gmail.com>
2019-12-20 14:14:36 -0500

    Implement ReadIntegerToBytes

--- jirachi-core/ByteFunctions.cs
+++ jirachi-core/ByteFunctions.cs
@@ -42,6 +42,57 @@ namespace jirachi_core {
             return byteVal;
         }
 
+        /// <summary>
+        /// Returns an array of bytes representing the provided integer.
+        /// </summary>
+        /// <param name="value">The integer to convert to bytes</param>
+        /// <param name="forcedByteCount">If the integer is fewer bytes than this parameter, the return bytes will be padded to this count</param>
+        /// <param name="bigEndian">Whether or not to return the bytes in Big Endian format</param>
+        /// <returns></returns>
+        public static byte[] ReadIntegerToBytes(int value, int forcedByteCount = 0, bool bigEndian = true) {
+            if(value < 0) {
+                throw new ArgumentOutOfRangeException("Only positive values can be converted to bytes");
+            }
+
+            byte[] returnBytes;
+            byte[] integerAsBytes = BitConverter.GetBytes(value); // This will always have a count of 4
+
+            int sigByteCount = Convert.ToInt32(Math.Ceiling(Math.Log(value + 1, 256)));
+            int returnByteCount;
+
+            if (forcedByteCount < 0 || forcedByteCount > 4) {
+                throw new ArgumentOutOfRangeException("Valid values for forcedByteCount are 0 to 4, inclusive");
+            }
+            else if (forcedByteCount == 0) {
+                if (value == 0) {
+                    returnByteCount = 1;
+                }
+                else {
+                    returnByteCount = sigByteCount;
+                }
+            }
+            else if (forcedByteCount < sigByteCount) {
+                throw new ArgumentOutOfRangeException(String.Format("You cannot squish {0} bytes into {1} bytes", sigByteCount, forcedByteCount));
+            }
+            else {
+                returnByteCount = forcedByteCount;
+            }
+            returnBytes = new byte[returnByteCount];
+
+            // If the integer bytes are in Big Endian, we will transform them to Little Endian for ease of copy
+            // This way we can just copy the first returnByteCount bytes, since it will ignore 
+            if(!BitConverter.IsLittleEndian) {
+                Array.Reverse(integerAsBytes);
+            }
+            Array.Copy(integerAsBytes, 0, returnBytes, 0, returnByteCount);
+            
+            // Now, if we want the output to be in Big Endian, we have to flip again
+            if(bigEndian) {
+                Array.Reverse(returnBytes);
+            }
+            return returnBytes;
+        }
+
         public static int ReadBinaryEncodedDecimal(byte[] bytes, int offset, int length, bool bigEndian = true) {
             int byteVal = 0;
             int bytesRead = 0;
